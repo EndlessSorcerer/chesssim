7b057d74a2dbd2ff570be2745a0de8afa26af65c
EndlessSorcerer <92722616+EndlessSorcerer@users.noreply.github.com>
2024-07-06 20:05:06 +0530

    ?

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 '''functionalities needed:
--Board containing 64 cells
--Each Piece's possible moves
 -Add castling
 -Implement En passant
+-Implement 50 move rule
+-Implement 3 repetitions
 '''
 class Cell:
     def __init__(self,x,y,piece):
@@ -23,12 +23,15 @@ class Piece:
         self.updatemoves()
     def kill(self):
         self.isalive=False
-    def move(self,board,x,y): #outdated
+    def move(self,game,x,y): #outdated
+        board=game.curboard
+        prev=game.prevstates[-1]
         if islegal(x,y):
             self.hasmoved=True
             if board.cells[x][y].piece!=None:
                 board.cells[x][y].piece.kill()
                 board.cells[x][y].piece=None
+            
             board.cells[self.x][self.y].piece=None
             board.cells[x][y].piece=self
         else:
@@ -37,7 +40,8 @@ class Rook(Piece):
     def __init__(self,label,x,y,color):
         super().__init__(label,x,y,color)
         self.symbol='R'
-    def updatemoves(self,board):
+    def updatemoves(self,game):
+        board = game.curboard
         a=[1,0,-1,0]
         b=[0,1,0,-1]
         for i in range(4):
@@ -56,7 +60,8 @@ class Bishop(Piece):
     def __init__(self,label,x,y,color):
         super().__init__(label,x,y,color)
         self.symbol='B'
-    def updatemoves(self,board):
+    def updatemoves(self,game):
+        board = game.curboard
         a=[1,1,-1,-1]
         b=[1,-1,1,-1]
         for i in range(4):
@@ -75,7 +80,8 @@ class Knight(Piece):
     def __init__(self,label,x,y,color):
         super().__init__(label,x,y,color)
         self.symbol='H'
-    def updatemoves(self,board):
+    def updatemoves(self,game):
+        board = game.curboard
         a=[1,1,-1,-1,2,2,-2,-2]
         b=[2,-2,2,-2,1,-1,1,-1]
         for i in range(8):
@@ -92,7 +98,8 @@ class Queen(Piece):
     def __init__(self,label,x,y,color):
         super().__init__(label,x,y,color)
         self.symbol='Q'
-    def updatemoves(self,board):
+    def updatemoves(self,game):
+        board = game.curboard
         a=[1,1,-1,-1,1,0,-1,0]
         b=[1,-1,1,-1,0,1,0,-1]
         for i in range(4):
@@ -112,7 +119,8 @@ class King(Piece):
     def __init__(self,label,x,y,color):
         super().__init__(label,x,y,color)
         self.symbol='K'
-    def updatemoves(self,board):
+    def updatemoves(self,game):
+        board = game.curboard
         a=[1,1,1,0,0,-1,-1,-1]
         b=[0,1,-1,1,-1,0,1,-1]
         for i in range(8):
@@ -136,11 +144,27 @@ class Pawn(Piece):
     def __init__(self,label,x,y,color):
         super().__init__(label,x,y,color)
         self.symbol='p'
-    def updatemoves(self,board): 
+    def updatemoves(self,game):
+        board = game.curboard
+        prev = game.prevstates[-1]
         if board.cells[self.x+(self.color)][self.y].piece==None:
             self.validmoves.add([self.x+(self.color)][self.y])
             if self.hasmoved==False and board.cells[self.x+2*(self.color)][self.y].piece==None:
                 self.validmoves.add([self.x+2*(self.color)][self.y])
+        if board.getcell(self.x+(self.color),self.y+1) and board.getcell(self.x+(self.color),self.y+1).piece!=None and board.getcell(self.x+(self.color),self.y+1).piece.color!=self.color:
+            self.validmoves.add([self.x+(self.color)][self.y+1])
+            self.targets.add(board.cells[self.x+(self.color)][self.y+1].piece)
+        if board.getcell(self.x+(self.color),self.y-1) and board.getcell(self.x+(self.color),self.y-1).piece!=None and board.getcell(self.x+(self.color),self.y-1).piece.color!=self.color:
+            self.validmoves.add([self.x+(self.color)][self.y-1])
+            self.targets.add(board.cells[self.x+(self.color)][self.y-1].piece)
+        if board.getcell(self.x,self.y+1) and board.getcell(self.x,self.y+1).piece!=None and board.getcell(self.x,self.y+1).piece.color!=self.color and isinstance(board.getcell(self.x,self.y+1).piece,Pawn):
+            if prev.getcell(self.x+2*(self.color),self.y+1) and prev.getcell(self.x+2*(self.color),self.y+1).piece.color==board.getcell(self.x,self.y+1).piece.color and prev.getcell(self.x+2*(self.color),self.y+1).piece.label==board.getcell(self.x,self.y+1).piece.label:
+                self.validmoves.add([self.x+(self.color)][self.y+1])
+                self.targets.add(board.cells[self.x][self.y+1].piece)
+        if board.getcell(self.x,self.y-1) and board.getcell(self.x,self.y-1).piece!=None and board.getcell(self.x,self.y-1).piece.color!=self.color and isinstance(board.getcell(self.x,self.y-1).piece,Pawn):
+            if prev.getcell(self.x+2*(self.color),self.y-1) and prev.getcell(self.x+2*(self.color),self.y-1).piece.color==board.getcell(self.x,self.y-1).piece.color and prev.getcell(self.x+2*(self.color),self.y-1).piece.label==board.getcell(self.x,self.y-1).piece.label:
+                self.validmoves.add([self.x+(self.color)][self.y-1])
+                self.targets.add(board.cells[self.x][self.y-1].piece)
         
 
 class Board:
@@ -151,38 +175,38 @@ class Board:
         self.reset()
     def reset(self):
         # figure out what to do with updatemoves
-        self.cells[0][0]=Cell(0,0,Rook("rook",0,0,0))
-        self.cells[0][1]=Cell(0,1,Knight("knight",0,1,0))
-        self.cells[0][2]=Cell(0,2,Bishop("bishop",0,2,0))
+        self.cells[0][0]=Cell(0,0,Rook("rook1",0,0,0))
+        self.cells[0][1]=Cell(0,1,Knight("knight1",0,1,0))
+        self.cells[0][2]=Cell(0,2,Bishop("bishop1",0,2,0))
         self.cells[0][3]=Cell(0,3,Queen("queen",0,3,0))
         self.cells[0][4]=Cell(0,4,King("king",0,4,0))
-        self.cells[0][5]=Cell(0,5,Bishop("bishop",0,5,0))
-        self.cells[0][6]=Cell(0,6,Knight("knight",0,6,0))
-        self.cells[0][7]=Cell(0,7,Rook("rook",0,7,0))
-        self.cells[1][0]=Cell(1,0,Pawn("pawn",1,0,0))
-        self.cells[1][1]=Cell(1,1,Pawn("pawn",1,1,0))
-        self.cells[1][2]=Cell(1,2,Pawn("pawn",1,2,0))
-        self.cells[1][3]=Cell(1,3,Pawn("pawn",1,3,0))
-        self.cells[1][4]=Cell(1,4,Pawn("pawn",1,4,0))
-        self.cells[1][5]=Cell(1,5,Pawn("pawn",1,5,0))
-        self.cells[1][6]=Cell(1,6,Pawn("pawn",1,6,0))
-        self.cells[1][7]=Cell(1,7,Pawn("pawn",1,7,0))
-        self.cells[7][0]=Cell(7,0,Rook("rook",7,0,1))
-        self.cells[7][1]=Cell(7,1,Knight("knight",7,1,1))
-        self.cells[7][2]=Cell(7,2,Bishop("bishop",7,2,1))
+        self.cells[0][5]=Cell(0,5,Bishop("bishop2",0,5,0))
+        self.cells[0][6]=Cell(0,6,Knight("knight2",0,6,0))
+        self.cells[0][7]=Cell(0,7,Rook("rook2",0,7,0))
+        self.cells[1][0]=Cell(1,0,Pawn("pawn1",1,0,0))
+        self.cells[1][1]=Cell(1,1,Pawn("pawn2",1,1,0))
+        self.cells[1][2]=Cell(1,2,Pawn("pawn3",1,2,0))
+        self.cells[1][3]=Cell(1,3,Pawn("pawn4",1,3,0))
+        self.cells[1][4]=Cell(1,4,Pawn("pawn5",1,4,0))
+        self.cells[1][5]=Cell(1,5,Pawn("pawn6",1,5,0))
+        self.cells[1][6]=Cell(1,6,Pawn("pawn7",1,6,0))
+        self.cells[1][7]=Cell(1,7,Pawn("pawn8",1,7,0))
+        self.cells[7][0]=Cell(7,0,Rook("rook1",7,0,1))
+        self.cells[7][1]=Cell(7,1,Knight("knight1",7,1,1))
+        self.cells[7][2]=Cell(7,2,Bishop("bishop1",7,2,1))
         self.cells[7][3]=Cell(7,3,Queen("queen",7,3,1))
         self.cells[7][4]=Cell(7,4,King("king",7,4,1))
-        self.cells[7][5]=Cell(7,5,Bishop("bishop",7,5,1))
-        self.cells[7][6]=Cell(7,6,Knight("knight",7,6,1))
-        self.cells[7][7]=Cell(7,7,Rook("rook",7,7,1))
-        self.cells[6][0]=Cell(6,0,Pawn("pawn",6,0,1))
-        self.cells[6][1]=Cell(6,1,Pawn("pawn",6,1,1))
-        self.cells[6][2]=Cell(6,2,Pawn("pawn",6,2,1))
-        self.cells[6][3]=Cell(6,3,Pawn("pawn",6,3,1))
-        self.cells[6][4]=Cell(6,4,Pawn("pawn",6,4,1))
-        self.cells[6][5]=Cell(6,5,Pawn("pawn",6,5,1))
-        self.cells[6][6]=Cell(6,6,Pawn("pawn",6,6,1))
-        self.cells[6][7]=Cell(6,7,Pawn("pawn",6,7,1))
+        self.cells[7][5]=Cell(7,5,Bishop("bishop2",7,5,1))
+        self.cells[7][6]=Cell(7,6,Knight("knight2",7,6,1))
+        self.cells[7][7]=Cell(7,7,Rook("rook2",7,7,1))
+        self.cells[6][0]=Cell(6,0,Pawn("pawn1",6,0,1))
+        self.cells[6][1]=Cell(6,1,Pawn("pawn2",6,1,1))
+        self.cells[6][2]=Cell(6,2,Pawn("pawn3",6,2,1))
+        self.cells[6][3]=Cell(6,3,Pawn("pawn4",6,3,1))
+        self.cells[6][4]=Cell(6,4,Pawn("pawn5",6,4,1))
+        self.cells[6][5]=Cell(6,5,Pawn("pawn6",6,5,1))
+        self.cells[6][6]=Cell(6,6,Pawn("pawn7",6,6,1))
+        self.cells[6][7]=Cell(6,7,Pawn("pawn8",6,7,1))
 
         for i in range(2,6):
             for j in range(0,8):
@@ -191,6 +215,10 @@ class Board:
             for j in range(0,8):
                 if self.cells[i][j].piece!=None:
                     self.activepieces.append(self.cells[i][j].piece)
+    def getcell(self,x,y):
+        if x<0 or y<0 or x>7 or y>7:
+            return None
+        return self.cells[x][y]
     def printboard(self):
         for i in range(0,8):
             for j in range(0,8):
@@ -205,7 +233,8 @@ class Board:
 
 class Game:
     def __init__(self):
-        self.board = Board()
+        self.curboard = Board()
+        self.prevstates = []
         self.whiteturn = True
     def start(self):
 
